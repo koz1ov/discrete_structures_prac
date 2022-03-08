9c7351d20dea631cb01b553b1a2f006bde6e7c28
Kozlov Alexander <kostebo98@mail.ru>
2022-03-09 00:28:29 +0300

    first hw

---
+++ get_decision.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from keras import models
+
+if __name__ == '__main__':
+    x = eval(open(f'{sys.argv[1]}', 'r').read())
+    x = np.array(list(map(float, x)))
+    model = models.load_model("result_model")
+    next_val = model.predict(x[-50:].reshape(1, -1))[0][0]
+    
+    if next_val > x[-1]:
+        print("BUY")
+    elif next_val < x[-1]:
+        print("SELL")
+    else:
+        print("DO NOTHING")
---
+++ pred_next_values.py
@@ -0,0 +1,20 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from keras import models
+
+if __name__ == '__main__':
+    x = eval(open(f'{sys.argv[1]}', 'r').read())
+    x = np.array(list(map(float, x)))
+    fig, ax = plt.subplots()
+    model = models.load_model("result_model")
+    x_pred = np.array([])
+    for i in range(5):
+        next_val = model.predict(np.concatenate((x[-50 + i:], x_pred)).reshape(1, -1))[0][0]
+        x_pred = np.append(x_pred, next_val)
+   
+    ax.plot(np.arange(1, 6), x[-5:], color='green')
+    ax.plot(np.arange(6, 11), x_pred, color='red')
+    plt.show()
